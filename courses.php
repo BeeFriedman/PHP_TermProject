19746cff3aa40357b916dda52d91f757c5214968
BeeFriedman <70110787+BeeFriedman@users.noreply.github.com>
2021-07-12 14:29:26 -0400

    Minor changes

--- courses.php
+++ courses.php
@@ -3,28 +3,27 @@
     $pageIdentifier = "courses"; 
     include "inc/header.php"; 
     require "config/db.php";
-    echo "<p>Courses you're enrolled in.</p>";
+    echo "<h1><u>Courses you're enrolled in.</u></h1>";
 
     $course_query = "SELECT courses.code, courses.name, courses.description FROM studentscourses JOIN courses ON courses.id = course_id where student_id = {$_SESSION["student_id"]};";
     $course_result = mysqli_query($conn, $course_query);
     $course_posts = mysqli_fetch_all($course_result, MYSQLI_ASSOC);
 ?>
+    <table>
+        <tr>
+            <th>Course Code</th>
+            <th>Course Name</th> 
+            <th>Description</th>
+        </tr>
 
-    <?php foreach($course_posts as $c_post): ?>
-        <table>
-            <tr>
-                <th>Course Code</th>
-                <th>Course Name</th> 
-                <th>Description</th>
-            </tr>
-            <tr>
-                <td><?php echo $c_post["code"]; ?></td>
-                <td><?php echo $c_post["name"]; ?></td>
-                <td><?php echo $c_post["description"]; ?></td>
-            </tr>
-        </table>
-
+        <?php foreach($course_posts as $c_post): ?>
+                <tr>
+                    <td><?php echo $c_post["code"]; ?></td>
+                    <td><?php echo $c_post["name"]; ?></td>
+                    <td><?php echo $c_post["description"]; ?></td>
+                </tr>
+        <?php endforeach; ?>
+    </table>
 <?php
-    endforeach;
     include "inc/footer.php"; 
 ?>
