313efca1ccfab54a5d304e9418ec1f6717f4ccb0
BeeFriedman <BeeFriedman@github.com>
2021-07-12 14:29:58 -0400

    minor changes

--- Inc/header.php
+++ Inc/header.php
@@ -25,13 +25,13 @@
         <title>ManageSchool</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/styles.css" type="text/css" />
-        <link rel="icon" href="assets//favicon.ico" />
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <link rel="icon" href="assets/favicon.ico" />
     </head>
     <body class="<?php echo $class ?>"> 
         <header class="header">
-            <img src="../assets/Logo.png" alt="Logo"/>
+            <img src="assets/Logo.png" alt="Logo"/>
         </header>
-        <?php include "../menu.php"; ?>
+        <?php include "menu.php"; ?>
         <hr>
 
