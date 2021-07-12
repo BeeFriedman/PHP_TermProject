3f2b1e30378ccf0851388b7afa468c3c4ec49bb7
BeeFriedman <BeeFriedman@github.com>
2021-07-12 11:00:00 -0400

    Added a config with database creds, added a table.

--- courses.php
+++ courses.php
@@ -2,16 +2,29 @@
     session_start();
     $pageIdentifier = "courses"; 
     include "Inc/header.php"; 
+    require "Config/db.php";
     echo "<p>Courses you're enrolled in.</p>";
 
-    $_SESSION["conn"] = mysqli_connect("localhost", "root", "123456", "manageschool");
     $course_query = "SELECT courses.code, courses.name, courses.description FROM studentscourses JOIN courses ON courses.id = course_id where student_id = {$_SESSION["student_id"]};";
-    $course_result = mysqli_query($_SESSION['conn'], $course_query);
-    $course_posts = mysqli_fetch_assoc($course_result);
+    $course_result = mysqli_query($conn, $course_query);
+    $course_posts = mysqli_fetch_all($course_result, MYSQLI_ASSOC);
+?>
 
-    foreach($course_posts as $c_post){
-        echo "$c_post";
-    }
+    <?php foreach($course_posts as $c_post): ?>
+        <table>
+            <tr>
+                <th>Course Code</th>
+                <th>Course Name</th> 
+                <th>Description</th>
+            </tr>
+            <tr>
+                <td><?php echo $c_post["code"]; ?></td>
+                <td><?php echo $c_post["name"]; ?></td>
+                <td><?php echo $c_post["description"]; ?></td>
+            </tr>
+        </table>
 
+<?php
+    endforeach;
     include "Inc/footer.php"; 
 ?>
--- submission.php
+++ submission.php
@@ -1,14 +1,13 @@
 <?php
     session_start();
     $pageIdentifier = "submission";
+    require "Config/db.php";
     include "Inc/header.php";
     
-    $_SESSION["conn"] = mysqli_connect("localhost", "root", "123456", "manageschool");
     $query = "SELECT id, username FROM authorizedusers WHERE username='$_POST[username]' AND password='$_POST[password]'";
-    $result = mysqli_query($_SESSION["conn"], $query);
+    $result = mysqli_query($conn, $query);
     $posts = mysqli_fetch_assoc($result);
     $_SESSION["student_id"] = $posts["id"];
-    print_r($posts);
 
     if(mysqli_num_rows($result) == 1){
         echo "<a href='index.php'>Dashboard</a>";
