868b0640cc55debbb052bc20c8c4d97e08027b35
BeeFriedman <70110787+BeeFriedman@users.noreply.github.com>
2021-07-15 03:54:25 -0400

    updated the registration form, then updated submission.php to store the data in the database

--- login.php
+++ login.php
@@ -3,16 +3,16 @@
     $_SESSION["page_identifier"] = "login";  
     include "inc/header.php"; 
 ?>
-    <div>
+    <div class="login">
         <form action="submission.php" name="login" method="POST">
             <input type="hidden" name="form_type" value="login"/>
             <input class="textBox" type="text" name="username" placeholder="Enter your username" maxlength="20"/>
             <input class="textBox" type="password" name="password" placeholder="Enter your password" maxlength="20"/><br><br>
             <input class="button" type="submit" value="Submit"/>
         </form>
-        <form class="register" action="register.php">
-            <input class="button" type="submit" value="New user? Register"/>
-        </form>
+        <button class="button_register">
+            <a href="register.php">New user? Register</a>
+        </button>
     </div>
 <?php include "inc/footer.php"; ?>
 
--- register.php
+++ register.php
@@ -1,25 +1,49 @@
 <?php
     $_SESSION["page_identifier"] = "register";
+    require "config/db.php";
     include "inc/header.php";
+
+    $get_majors_query = "SELECT name FROM majors";
+    $raw_majors_result = mysqli_query($conn, $get_majors_query);
+    $majors_result_array = mysqli_fetch_all($raw_majors_result, MYSQLI_ASSOC);
+
+    $get_campuses_query = "SELECT name, state, country FROM campuses";
+    $raw_campuses_result = mysqli_query($conn, $get_campuses_query);
+    $campuses_result_array = mysqli_fetch_all($raw_campuses_result, MYSQLI_ASSOC);
 ?>
-    <div>
+    <div class="form">
         <h2><b>Enter Student Information</b></h2>
         <form class="student_info" action="submission.php" name="studentInfo" method="POST">
             <input type="hidden" name="form_type" value="registration"/>       
-            <input class="textBox" type="text" name="username" placeholder="Username" maxlength="20"/>
-            <input class="textBox" type="password" name="password" placeholder="Password" maxlength="20"/><br><br>
-            <input class="textBox" type="text" name="first" placeholder="First Name" maxlength="20"/>
-            <input class="textBox" type="text" name="last" placeholder="Last Name" maxlength="20"/><br><br>
-            <input class="textBox" type="tel" name="phone" placeholder="Phone Number" maxlength="12"/>
-            <input class="textBox" type="email" name="email" placeholder="Email" maxlength="25"/><br><br>
-            <select class="select" name="type">
+            <input class="textBox" type="text" name="username" placeholder="Username" maxlength="20" required/>
+            <input class="textBox" type="password" name="password" placeholder="Password" maxlength="20" required/><br><br>
+            <input class="textBox" type="text" name="first" placeholder="First Name" maxlength="20" required/>
+            <input class="textBox" type="text" name="last" placeholder="Last Name" maxlength="20" required/><br><br>
+            <input class="textBox" type="tel" name="phone" placeholder="Phone Number" maxlength="12"required/>
+            <input class="textBox" type="email" name="email" placeholder="Email" maxlength="25" required/><br><br>
+            <select class="select" name="type" required>
+                <option selected disabled>Select user type</option>
                 <option>New Student</option>
                 <option>External Transfer</option>
                 <option>Internal Transfer</option>
                 <option>Admin</option>
             </select>
+            <select class="select" name="major" required>
+                <option selected disabled>Select a major<t/option>
+                <?php foreach($majors_result_array as $major): ?>
+                <option><?php echo $major["name"]; ?></option>
+                <?php endforeach; ?>
+            </select>
             <br><br>
-            <div class="radio">
+            <select class="select" id="campus" name="campuses" required>
+                <option selected disabled>Select a campus<t/option>
+                <?php foreach($campuses_result_array as $campus): ?>
+                <option><?php echo $campus["name"] . ", " . (($campus["state"] != "NA")? $campus["state"] . ", " : "") . 
+                            $campus["country"]; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <br><br>
+            <div class="radio" name="level">
                 <label><b><u>Education Level</u></b></label><br>
                 <label>Non</label>
                 <input type ="radio" name="level" value="Non" checked/><br>
